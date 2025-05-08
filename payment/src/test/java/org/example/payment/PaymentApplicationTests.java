c5171d721796438ea6bd96bfd436ceb200657b0e
bolohonov <bolohonov@list.ru>
2025-05-08 13:50:30 +0400

    add tests for payment service & repo

--- payment/src/test/java/org/example/payment/PaymentApplicationTests.java
+++ payment/src/test/java/org/example/payment/PaymentApplicationTests.java
@@ -2,6 +2,7 @@ package org.example.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.TestcontainersConfiguration;
